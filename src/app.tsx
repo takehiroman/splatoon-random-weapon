7b37857d4db7fd39d3d0bfd7c593bc9171b2bdba
takehiroman <takehiro.tsu@gmail.com>
2021-12-23 10:50:10 +0900

    add tailwind

--- src/app.tsx
+++ src/app.tsx
@@ -5,16 +5,6 @@ export function App() {
     <>
       <Logo />
       <p>Hello Vite + Preact!</p>
-      <p>
-        <a
-          class="link"
-          href="https://preactjs.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Preact
-        </a>
-      </p>
     </>
   )
 }

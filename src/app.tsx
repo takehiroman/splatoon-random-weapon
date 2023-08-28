01db65a681b72e78b6a3468bca78a6ae276ad5cd
津崎　豪宏 <tsuzakitakehiro@LAB-N2819R.local>
2023-08-28 17:22:29 +0900

    Appコンポーネント修正

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,30 @@
-import { Logo } from './components/logo'
-import {h,Fragment} from 'preact'
+import { h, Fragment } from 'preact'
 import { Button } from './components/Button'
+import { CardList } from './components/CardList'
 export function App() {
+  const cards = [
+    {
+      title: 'Preact',
+      description: 'Fast 3kB alternative to React with the same modern API.',
+    },
+    {
+      title: 'Vite',
+      description: 'Native-ESM powered frontend dev server.',
+    },
+    {
+      title: 'TypeScript',
+      description: 'Typed JavaScript at Any Scale.',
+    },
+  ]
   const handleClick = () => {
     alert('Hello!')
   }
   return (
     <>
-      <Logo />
-      <p>Hello Vite + Preact!</p>
-      <Button text="Click me!" onClick={handleClick} />
+      <CardList cards={cards} />
+      <div class="flex m-4 justify-center">
+        <Button text="Click me!" onClick={handleClick} />
+      </div>
     </>
   )
 }

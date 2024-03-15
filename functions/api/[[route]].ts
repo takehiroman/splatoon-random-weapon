0842ba8e36bc04272b34a2087477a20101fd6efa
津崎 豪宏 <tsuzakitakehiro@team-lab.com>
2024-03-15 19:40:05 +0900

    feat: ファイル名前変更

--- functions/api/[[route]].ts
+++ functions/api/[[route]].ts
@@ -1,6 +1,7 @@
 import { D1Database } from '@cloudflare/workers-types'
 import { handle } from 'hono/cloudflare-pages'
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 
 export const runtime = 'edge'
 
@@ -9,6 +10,18 @@ type Bindings = {
 }
 
 const app = new Hono<{ Bindings: Bindings }>().basePath('/api')
+
+app.use(
+  '*',
+  cors({
+    origin: '*',
+    allowHeaders: ['Content-Type', 'Authorization'],
+    allowMethods: ['POST', 'GET', 'OPTIONS'],
+    exposeHeaders: ['Content-Length'],
+    maxAge: 600,
+    credentials: true,
+  })
+)
 
 // Accessing D1 is via the c.env.YOUR_BINDING property
 app.get('/weapons', async (c) => {
